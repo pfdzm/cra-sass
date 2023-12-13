06661aab5b09c87b41d46fbea06f1d2bd50a8c26
Pablo Fernandez Michel <pfdzm@users.noreply.github.com>
2023-12-13 14:11:09 +0100

    add form to create new post

--- src/FetchData.js
+++ src/FetchData.js
@@ -1,17 +1,40 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function FetchData() {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     const fetchPosts = async () => {
-      const resp = await fetch("/posts");
-      if (!resp.ok) {
-        return;
-      }
-      setPosts(await resp.json());
+      // using Fetch API
+      // const resp = await fetch("/posts");
+      // if (!resp.ok) {
+      //   return;
+      // }
+      // setPosts(await resp.json());
+
+      // using Axios
+      const resp = await axios.get("/posts");
+      setPosts(resp.data);
     };
+
     fetchPosts();
   }, []);
+
+  const [newPost, setNewPost] = useState(null);
+
+  useEffect(() => {
+    if (newPost === null) {
+      return;
+    }
+
+    const addPost = async () => {
+      await axios.post("/posts", newPost);
+      setPosts((posts) => [...posts, newPost]);
+      setNewPost(null);
+    };
+
+    addPost();
+  }, [newPost]);
 
   return (
     <div className="posts">
@@ -23,6 +46,23 @@ export default function FetchData() {
           </li>
         ))}
       </ol>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const data = new FormData(e.target);
+
+          setNewPost({
+            title: data.get("title"),
+            author: data.get("author"),
+            id: Date.now(),
+          });
+        }}
+      >
+        <input required type="text" name="title" placeholder="Title" />
+        <input required type="text" name="author" placeholder="Author" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
