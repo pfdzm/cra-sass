ef1a4cc61bec9c004c0d8d04b2858a60c96fdc27
Pablo Fernandez Michel <pfdzm@users.noreply.github.com>
2023-12-13 14:01:53 +0100

    add some base container style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Outlet />
+      <div className="content">
+        <Outlet />
+      </div>
     </div>
   );
 }
